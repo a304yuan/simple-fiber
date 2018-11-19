5d8a30c73e25718356deab8b444d08122d50253a
a304yuan <a304yuanyuan@gmail.com>
2018-11-19 18:03:22 +0800

    add basic register storage

--- fiber.h
+++ fiber.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdatomic.h>
 #include <threads.h>
 
 typedef struct fiber fiber;
@@ -19,6 +21,9 @@ struct fiber {
     fiber_start_func func;
     void * arg;
     size_t frame_size;
+    struct {
+        uint64_t ax, bx, cx, dx, si, di;
+    } registers;
     fiber_status status;
     fiber * next;
     void * frame;
@@ -32,24 +37,44 @@ extern fiber * fiber_create(fiber_start_func func, void * arg);
     do { \
         if (fb->frame) { \
             void * _bp, * _sp; \
-            __asm__( \
+            __asm__ __volatile__ ( \
                 "mov %%rbp, %0\n\t" \
                 "mov %%rsp, %1\n\t" \
                 : "=r"(_bp), "=r"(_sp) \
             ); \
-            memcpy(_bp - fb->frame_size, fb->frame, fb->frame_size); \
+            atomic_thread_fence(memory_order_acq_rel); \
+            memcpy(_sp, fb->frame, fb->frame_size); \
+            atomic_thread_fence(memory_order_acq_rel); \
+            __asm__ __volatile__ ( \
+                "mov %0, %%rax\n\t" \
+                "mov %1, %%rbx\n\t" \
+                "mov %2, %%rcx\n\t" \
+                "mov %3, %%rdx\n\t" \
+                "mov %4, %%rsi\n\t" \
+                "mov %5, %%rdi\n\t" \
+                :: "r"(fb->registers.ax), "r"(fb->registers.bx), "r"(fb->registers.cx), "r"(fb->registers.dx), "r"(fb->registers.si), "r"(fb->registers.di) \
+            ); \
+            atomic_thread_fence(memory_order_acq_rel); \
             goto YIELD_POINT; \
         } \
     } while (0)
 
 #define fiber_yield(fb) \
     do { \
+        atomic_thread_fence(memory_order_acq_rel); \
         void * _bp, * _sp; \
-        __asm__( \
+        __asm__ __volatile__ ( \
             "mov %%rbp, %0\n\t" \
             "mov %%rsp, %1\n\t" \
-            : "=r"(_bp), "=r"(_sp) \
+            "mov %%rax, %2\n\t" \
+            "mov %%rbx, %3\n\t" \
+            "mov %%rcx, %4\n\t" \
+            "mov %%rdx, %5\n\t" \
+            "mov %%rsi, %6\n\t" \
+            "mov %%rdi, %7\n\t" \
+            : "=r"(_bp), "=r"(_sp), "=r"(fb->registers.ax), "=r"(fb->registers.bx), "=r"(fb->registers.cx), "=r"(fb->registers.dx), "=r"(fb->registers.si), "=r"(fb->registers.di) \
         ); \
+        atomic_thread_fence(memory_order_acq_rel); \
         size_t frame_size = _bp - _sp; \
         if (fb->frame_size < frame_size) { \
             if (fb->frame) { \
@@ -67,6 +92,7 @@ extern fiber * fiber_create(fiber_start_func func, void * arg);
 
 #define fiber_exit(fb) \
     do { \
+        atomic_thread_fence(memory_order_acq_rel); \
         fb->status = FIBER_EXITED; \
         return FIBER_EXITED; \
     } while (0)
